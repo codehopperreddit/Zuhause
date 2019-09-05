01a62c2244f028a2e428612cf57b18100485fe3a
Sagnik Chatterjee <codehopperreddit>
2019-09-05 20:38:07 +0530

    Added a rudimentary php script
    >Php script: fetches data from table
    >added hta access to hide .php extensions
    > Added running instructions to readme

---
+++ forsale.php
@@ -0,0 +1,44 @@
+
+<html>
+    <head>
+        <title>Lists the for sale properties</title>
+        <script>
+           
+
+        </script>
+    </head> 
+    <body>
+            <?php
+            $dbhost = 'localhost:3306';
+            $dbuser = 'admin';
+            $dbpass = 'securepassword';
+            
+            $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+            
+            if(! $conn ) {
+               die('Could not connect: ' . mysqli_error());
+            }
+            
+            $sql = 'SELECT house_number, owner_name, rate FROM properties';
+            mysqli_select_db($conn,'zuhause');
+            $retval = mysqli_query($conn,$sql);
+            
+            if(! $retval ) {
+               die('Could not get data: ' . mysqli_error($con));
+            }
+            
+            while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
+               echo "House Number :{$row['house_number']}  <br> ".
+                  "Owner Name : {$row['owner_name']} <br> ".
+                  "Rate Per Month : {$row['rate']} <br> ".
+                  "--------------------------------<br>";
+            }
+            
+            echo "Fetched data successfully\n";
+            
+            mysqli_close($conn);
+         ?>
+         
+
+    </body>  
+</html>
