f943adf60326536cb1c0077f63a0bd9fa1fb9797
Sagnik Chatterjee <32343708+codehopperreddit@users.noreply.github.com>
2019-10-01 00:44:42 +0530

    Added prepare satement and password hashing
    > Replaced all SQL statements with prepared statements
    >made changes to save only the hash to the database
    >made changes to check the hash value with the input password

--- SignIn.php
+++ SignIn.php
@@ -15,8 +15,8 @@
 
 //        $email = $_POST["email"];
 //		$pass = $_POST["password"];
-		$email = mysqli_real_escape_string($conn, $_POST['email']);
-      	$pass = mysqli_real_escape_string($conn, $_POST['password']);
+		$email = $_POST['email'];
+      	$pass = $_POST['password'];
 
 		echo "Email : ". $email;
 		echo "<br/>";
@@ -24,16 +24,31 @@
 		echo "<br/>";
 
         echo 'Connected successfully<br>';
-        $sql = "SELECT * FROM user where username = '$email' and password = '$pass'";
-
-        $result = mysqli_query($conn, $sql);
+        $sql = $conn->prepare('SELECT name,password FROM user where username = ?'); //Select * is exploitable
+        if($sql !== FALSE) 
+        {
+            $sql->bind_param('s', $email);
+        }
+        else
+        {
+            die('prepare() failed: ' . htmlspecialchars($conn->error)); //dev only remove after
+            
+        }
+        $sql->execute();
+    
+        $sql->store_result();
+        
+        
+        
+        /*$result = mysqli_query($conn, $sql);
       	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-//      	$active = $row['active'];
+      	$active = $row['active'];
       	$count = mysqli_num_rows($result);
 
       	echo $count. "<br/>";
 
         if ($count == 1) {
+       
             while($row = mysqli_fetch_assoc($result)) {
                echo "Name: " . $row["username"]. "<br>";
                echo "Password: " . $row["password"]. "<br>";
@@ -51,4 +66,27 @@
      }
 
 //mkm@gmail.com
+*/
+
+if ($sql->num_rows > 0) 
+{
+    $sql->bind_result($name,$hash);
+    $sql->fetch();
+    
+    if (password_verify($pass, $hash)) //verifying password with hashed value
+    {
+        echo "Name: " . $name. "<br>";
+        echo "Password: " . $hash. "<br>";
+    } else 
+    {
+        echo 'Incorrect password or username!';    //wrong password
+    }
+} 
+else 
+{
+    echo 'Incorrect username or password!';    //wrong username  
+                                                //ideally there shouldn't be any difference in the error message to prevent bruteforcing
+}
+$sql->close();
+
 ?>
--- SignUp.php
+++ SignUp.php
@@ -9,29 +9,59 @@
 
         $conn = new mysqli($servername, $username, $password, $dbname);
 
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
+        if(! $conn ) 
+        {
+            die("Connection failed: " . mysqli_error($conn));
+        }
+        if ($stmt = $conn->prepare('SELECT id FROM user WHERE username = ?')) 
+        {
+            
+            $stmt->bind_param('s', $_POST['email']);
+            $stmt->execute();
+        
+            if ($stmt->num_rows > 0) 
+            {
+                header('Location: '.$uri.'useralreadyexists.html');
+            }
         }
 
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $name = mysqli_real_escape_string($conn, $_POST['name']);
-        $pass = mysqli_real_escape_string($conn, $_POST['password']);
+        $email = $_POST['email'];
+        $name =$_POST['name'];
+        $password = $_POST['password'];
 
 //        echo "Name : ". $name. "<br/>";
 //        echo "Email : ". $email. "<br/>";
 //        echo "Password : ". $pass. "<br/>";
 
         echo 'Connected successfully<br>';
-        $sql = "INSERT INTO user(name, username, password) VALUES('$name', '$email', '$pass')";
+        
+        //Pasword hashing start
+        $options = [
+            'cost' => 12,
+                    ];   //salt is added randomly (this is prefered and standard)
+        $hash=password_hash($password,PASSWORD_BCRYPT, $options);
+        $password="password";//replacing data 
 
-        $result = mysqli_query($conn, $sql);
-        echo $result. '<br/>';
 
-        if($result){
-            echo "New user added successfully";
-            header('Location: index.html');
+        $sql = $conn->prepare("INSERT INTO user(name, username, password) VALUES(?,?,?)"); //Replaced with prepared staement
+                    
+        //Please be careful, the password column has to be varchar with atleast 200 size (250 recommended)
+        
+        if($sql !== FALSE) 
+        {
+             $sql->bind_param('sss',$name,$email,$hash);
         }
-        } else {
-            echo 'query error: '. mysqli_error($conn);
+        else
+        {
+            die('prepare() failed: ' . htmlspecialchars($conn->error)); //dev only remove after
+            
         }
+        
+
+        $sql->execute();    //use execute() for prepared statements
+
+
+        $sql->close();
+
+
 ?>
